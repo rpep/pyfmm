755275fc64c71dc6363b609d5d06335be9b3b724
Ryan Pepper <ryan.pepper@soton.ac.uk>
2017-03-14 08:13:11 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(name='spinfmm',
+      version='0.0.1'
+      description='Python implementation of Fast Multipole Method for spin dynamics',
+      author='Ryan Pepper',
+      author_email='ryan.pepper@soton.ac.uk',
+      url='www.github.com/ryanpepper/spinfmm',
+      packages=['spinfmm'],
+      install_requires = [
+        'pytest >= 2.8',
+        'numpy',
+        'hypothesis'
+      ],
+      classifiers = [
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: BSD',
+      ],
+)
