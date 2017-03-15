e1137526199b6d5a989ed00dec8770442d3adde7
ryanpepper <ryan.pepper1@gmail.com>
2017-03-15 15:29:35 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
-setup(name='spinfmm',
-      version='0.0.1'
+setup(name='pyfmm',
+      version='0.0.1',
       description='Python implementation of Fast Multipole Method for spin dynamics',
       author='Ryan Pepper',
       author_email='ryan.pepper@soton.ac.uk',
       url='www.github.com/ryanpepper/spinfmm',
-      packages=['spinfmm'],
+      packages=['pyfmm'],
       install_requires = [
         'pytest >= 2.8',
         'numpy',
