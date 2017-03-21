60e9ad4e8c27d585b55ae25888ed2cd63e6bceb2
Ryan Pepper <ryan.pepper@soton.ac.uk>
2017-03-21 16:42:54 +0000

    Add a test which checks the P2P, M2M approximation for a target a long distance away

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -46,3 +46,34 @@ def test_P2P_M2M(N):
     n._M2M()
     assert sum(n.M[9:, 0]) == N
     assert n.M[0, 0] == N
+
+
+@given(st.integers(50, 100))
+def test_P2P_M2M_approx(N):
+    r = np.random.uniform(0, 1, (N, 3))
+    q = np.ones(N)
+    n = NeighbourTree(r, q, maxlevel=2)
+    n._P2M()
+    n._M2M()
+    target = np.array([100.0, 100.0, 100.0])
+    phi_actual = 0
+    for rv in r:
+        dr = target - rv
+        norm = np.sqrt(dr.dot(dr))
+        phi_actual += 1/norm
+
+
+    drv = target - np.array([0.5, 0.5, 0.5])
+    dr = np.sqrt(drv.dot(drv))
+    dx, dy, dz = drv
+    dr3 = dr**3
+    dr5 = dr**5
+    
+    weights = np.array([1/dr, -dx/dr3, -dy/dr3, -dz/dr3, 3*dx**2/dr5 - 1/dr3, 
+                    3*dy**2/dr5 - 1/dr3, 3*dz**2/dr5 - 1/dr3, 3*dx*dy/dr5,
+                    3*dy*dz/dr5, 3*dz*dx/dr5])
+
+    phi_approx = np.dot(n.M[0], weights)
+    diff = np.sqrt((phi_approx - phi_actual)**2)/phi_actual
+    assert diff <= 1e-3
+    #assert 2 == 1, 'd = {}'.format(diff)
